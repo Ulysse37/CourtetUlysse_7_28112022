c29765a305124bdd1e5a8eeaa5e0b3a89e841b22
Ulysse37 <ulysse.courtet@hotmail.fr>
2023-01-15 17:35:33 +0100

    Ajout d'une partie du style des recettes

--- js/index.js
+++ js/index.js
@@ -4,25 +4,66 @@
 function createRecipesStructure(data) {
 
   let recipesSection                  = document.querySelector(".recipes-list");
-  
+  recipesSection.style.display        = "flex";
+  recipesSection.style.flexWrap       = "wrap";
+  recipesSection.style.flexDirection  = "row";
+  recipesSection.style.justifyContent = "center";
+
   let recipesElt                      = document.createElement("li");
+  recipesElt.style.width              = "25%";
+  recipesElt.style.display            = "flex";
+  recipesElt.style.flexDirection      = "column";
+  recipesElt.style.margin             = "0rem 3rem 0rem 3rem";
   recipesSection.appendChild(recipesElt);
 
+  let recipesLink                     = document.createElement("a");
+  recipesLink.href                    = "#";
+  recipesLink.textDecoration          = "none";
+  recipesElt.appendChild(recipesLink);
+
   let figureElt                       = document.createElement("figure");
-  recipesElt.appendChild(figureElt);
+  figureElt.style.borderRadius        = "8px";
+  figureElt.style.backgroundColor     = "#A4A4A4";
+  recipesLink.appendChild(figureElt);
+
+  let figureBackground                = document.createElement("div");
+  figureBackground.style.height       = "12rem";
+  figureElt.appendChild(figureBackground);
 
   let figcaptionElt                   = document.createElement("figcaption");
+  figcaptionElt.style.display         = "flex";
+  figcaptionElt.style.flexWrap        = "wrap";
+  figcaptionElt.style.flexDirection   = "row";
+  figcaptionElt.style.justifyContent  = "space-between";
+  figcaptionElt.style.borderRadius    ="0 0 8px 8px";
+  figcaptionElt.style.backgroundColor = "#E6E6E6";
   figureElt.appendChild(figcaptionElt);
   // NAME
   let nameElt                         = document.createElement("h2");
   nameElt.innerText                   = data.name;
+  nameElt.style.display               = "flex";
+  nameElt.style.justifyContent        = "space-between";
+  nameElt.style.width                 = "100%";
+  nameElt.style.margin                = "1rem";               
+  nameElt.style.fontSize              = "150%";
   figcaptionElt.appendChild(nameElt);
-  // TIME
-  let timeElt                         = document.createElement("p");
-  timeElt.innerText                   = data.time + " min";
-  figcaptionElt.appendChild(timeElt);
 
+//Todo Les appendChild sont-ils good practices ???
+  // TIME 
+ let timeIcon                         = document.createElement("i");
+  timeIcon.classList.add("fa-regular");
+  timeIcon.classList.add("fa-clock");
+  nameElt.appendChild(timeIcon);
+  // TIME
+  let timeElt                         = document.createElement("span");
+  timeElt.innerText                   = data.time + " min";
+  timeElt.style.marginLeft            = "0.5rem";
+  timeElt.style.fontFamily            = "Latto";
+  timeIcon.appendChild(timeElt);
+   
+// INGREDIENTS, QUANTITES ET UNITES 
   let ingredientsList                 = document.createElement("ul");
+  ingredientsList.style.margin = "0 0 1rem 1rem";
   figcaptionElt.appendChild(ingredientsList);
 
   let ingredients     = []; 
@@ -44,28 +85,30 @@ function createRecipesStructure(data) {
   // DESCRIPTION
   let descriptionElt  = document.createElement("p");
   descriptionElt.innerText = data.description;
+  descriptionElt.style.margin = "0 1rem 1rem 0";
   figcaptionElt.appendChild(descriptionElt);
 
-  console.log(data);
+  //console.log(data);
 }
 
 function displayRecipes(recipes) {
-
+  
   for (let i = 0; i < recipes.length; i++) {
     
     createRecipesStructure(recipes[i]);
-    
+
   }
 }
 
 function init() {
 
   displayRecipes(recipes);
+
 }
 
 init();
 
-//console.log(recipesSection);
+
 
 /*function getRecipes() {
     
