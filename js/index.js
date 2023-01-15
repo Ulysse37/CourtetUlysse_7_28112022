81328b7492ced5dbc456efd8b82fc41d767bf538
Ulysse37 <ulysse.courtet@hotmail.fr>
2023-01-15 15:10:09 +0100

    Réagencement syntaxe des unités des ingrédients des recettes

--- js/index.js
+++ js/index.js
@@ -3,41 +3,42 @@
 
 function createRecipesStructure(data) {
 
-  let recipesSection  = document.querySelector(".recipes-list");
+  let recipesSection                  = document.querySelector(".recipes-list");
   
-  let recipesElt      = document.createElement("li");
+  let recipesElt                      = document.createElement("li");
   recipesSection.appendChild(recipesElt);
 
-  let figureElt       = document.createElement("figure");
+  let figureElt                       = document.createElement("figure");
   recipesElt.appendChild(figureElt);
 
-  let figcaptionElt   = document.createElement("figcaption");
+  let figcaptionElt                   = document.createElement("figcaption");
   figureElt.appendChild(figcaptionElt);
   // NAME
-  let nameElt       = document.createElement("h2");
-  nameElt.innerText = data.name;
+  let nameElt                         = document.createElement("h2");
+  nameElt.innerText                   = data.name;
   figcaptionElt.appendChild(nameElt);
   // TIME
-  let timeElt         = document.createElement("p");
-  timeElt.innerText = data.time + " min";
+  let timeElt                         = document.createElement("p");
+  timeElt.innerText                   = data.time + " min";
   figcaptionElt.appendChild(timeElt);
 
-  let ingredientsList = document.createElement("ul");
+  let ingredientsList                 = document.createElement("ul");
   figcaptionElt.appendChild(ingredientsList);
 
   let ingredients     = []; 
   for (let i = 0; i < data.ingredients.length; i++) {
       ingredients[i]  = document.createElement("li");
-      if(data.ingredients[i].unit === "cuillères à soupe") {
-        ingredients[i].innerText = data.ingredients[i].ingredient + ": " + data.ingredients[i].quantity + " " + data.ingredients[i].unit;
+      if(data.ingredients[i].unit         === "cl") { // Ajout syntaxe dans le cas des ml/cl, des autres unités ou de leurs absences.
+        ingredients[i].innerText = data.ingredients[i].ingredient + " : " + data.ingredients[i].quantity + data.ingredients[i].unit;
+      } else if(data.ingredients[i].unit  === "ml") {
+        ingredients[i].innerText = data.ingredients[i].ingredient + " : " + data.ingredients[i].quantity + data.ingredients[i].unit;
       } else if(data.ingredients[i].unit) {
-        ingredients[i].innerText = data.ingredients[i].ingredient + ": " + data.ingredients[i].quantity + data.ingredients[i].unit;
-      } else if(data.ingredients[i].quantity){
-        ingredients[i].innerText = data.ingredients[i].ingredient + ": " + data.ingredients[i].quantity;
+        ingredients[i].innerText = data.ingredients[i].ingredient + " : " + data.ingredients[i].quantity + " " + data.ingredients[i].unit;
+      } else if(data.ingredients[i].quantity) {
+        ingredients[i].innerText = data.ingredients[i].ingredient + " : " + data.ingredients[i].quantity;
       } else {
         ingredients[i].innerText = data.ingredients[i].ingredient;
       }
-      
       ingredientsList.appendChild(ingredients[i]);
   }
   // DESCRIPTION
@@ -46,9 +47,6 @@ function createRecipesStructure(data) {
   figcaptionElt.appendChild(descriptionElt);
 
   console.log(data);
-
-  //Todo Ajouter le style des éléments ici
-
 }
 
 function displayRecipes(recipes) {
