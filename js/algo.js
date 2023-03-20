3d4ea37bffbc8799a1e6476f0774dac9a7ec6f8d
Ulysse37 <ulysse.courtet@hotmail.fr>
2023-03-20 09:40:09 +0100

    Ajout ID sur input + fonction eventlistener

--- js/algo.js
+++ js/algo.js
@@ -1,5 +1,31 @@
 "use strict";
 
+console.log(recipes);
 console.log(uniqueIngredientArray);
 console.log(uniqueApplianceArray);
 console.log(uniqueUstensilArray);
+
+
+function searchListener() {
+    let searchBar = document.getElementById("search-bar");
+
+    searchBar.addEventListener("input", (e) => {
+        let searchValue = e.target.value;
+        console.log(searchValue);
+    })
+    
+}
+searchListener();
+
+function filterByName() {
+    for (let i = 0; i < recipes.length; i++) {
+        let recipesName = recipes[i].name
+
+        if (recipesName.match(searchValue)) {
+            return true;
+        }
+        //console.log(recipesName);
+    }
+}
+//filterByName();
+
